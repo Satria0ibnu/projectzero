7a70a2fc8f431648bcf674e11a8d62c8a8384dc9
Satria Ibnu Pamungkas <satria0ibnu@gmail.com>
2024-08-16 15:36:28 +0700

    build 1st component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
+import Post from './components/Post'
 
+
+// Many JavaScript and React projects follow the Airbnb JavaScript Style Guide, 
+// which prefers single quotes for strings, except to avoid escaping
 function App() {
   
 
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+  return  <Post/>;
 }
 
 export default App
---
+++ src/components/Post.jsx
@@ -0,0 +1,11 @@
+function Post() {
+    return (
+        <div>
+            <p>Satria Ibnu</p>
+            <p>Nice React-ion</p>
+        </div>
+    )
+
+}
+
+export default Post;
