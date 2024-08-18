2f5061faeda0d7a62b2a53d97144afeccbee6f2c
Satria Ibnu Pamungkas <satria0ibnu@gmail.com>
2024-08-18 17:22:56 +0700

    lifting the state to other component

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,30 +1,26 @@
-import { useState } from 'react';
 // react has many built in function start with use.
 // that is considered react hooks (we can build our own react hooks btw)
 import styles from './NewPost.module.css'
 
-function NewPost() {
-    const [enteredBodyText, setEnteredBodyText] = useState();
+function NewPost(props) {
     // useState() contain an array with 2 value;
     // the first one is value of the current state
     // the second one is function to update the state
 
-    function changeBodyHandler(event) {
-        setEnteredBodyText(event.target.value);
-
-    }
+    //props.onBodyChange is declared in PostList.jsx
+    //we can name up that onBodyChange
 
   return (
     <form className={styles.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
+        <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
+        
         
       </p>
-      <p>{enteredBodyText}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange}/>
       </p> 
     </form>
   );
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,14 +1,34 @@
 import Post from './Post'
 import styles from './PostList.module.css'
 import NewPost from './NewPost';
+import { useState } from 'react';
+
 
 function PostList() {
+
+    const [enteredBodyText, setEnteredBodyText] = useState();
+    const [enteredAuthorText, setEnteredAuthorText] = useState();
+
+    // useState() contain an array with 2 value;
+    // the first one is value of the current state
+    // the second one is function to update the state
+
+    function changeBodyHandler(event) {
+        setEnteredBodyText(event.target.value);
+
+    }
+
+    function changeAuthorHandler(event) {
+        setEnteredAuthorText(event.target.value);
+
+    }
     return(
         //empty element or fragment <> to make it work with more content
+        //we pass the props to NewPost
          <>  
-            <NewPost/>
+            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler}/>
             <ul className={styles.posts}>
-                <Post author="Satria" body="Nice React-ion"/>
+                <Post author={enteredAuthorText} body={enteredBodyText}/>
                 <Post author="Ibnu Pamungkas" body="Why you cant react"/>
             </ul>
         </>
