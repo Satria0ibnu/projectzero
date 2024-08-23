413e38c0087d7b89fa1f9bc25d4d23dacb2603f3
Satria Ibnu Pamungkas <satria0ibnu@gmail.com>
2024-08-23 19:33:54 +0700

    add router

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -6,6 +6,8 @@ import Modal from './Modal';
 
 
 function PostList({isPosting, onStopPosting}) {
+    const [isFetching, setIsFetching] = useState(false);
+
     //updating the state causes component function got executed again
 
      // we send get request  then unpack data from response
@@ -15,10 +17,14 @@ function PostList({isPosting, onStopPosting}) {
     // by use empty array it tells, this component executed only once at first
     useEffect(() => { 
         async function fetchPosts() {
+            setIsFetching(true);
             const response = await fetch('http://localhost:8080/posts');
             const resData = await response.json();
+            if (!response.ok) {
+                throw new Error('Failed to fetch posts');
+            }
             setPosts(resData.posts)
-
+            setIsFetching(false);
         }
         fetchPosts();
         }, []);
@@ -73,9 +79,29 @@ function PostList({isPosting, onStopPosting}) {
         //we pass the props to NewPost
          <>  
         {modalContent}
-        <ul className={styles.posts}>
-            {posts.map((post) => <Post key={post.body} author={post.author} body={post.body}/> )}
-        </ul>
+        {!isFetching && posts.length > 0 ? 
+            <ul className={styles.posts}>
+                {posts.map((post) => 
+                    <Post key={post.body} 
+                    author={post.author} 
+                    body={post.body}/> )}
+            </ul>
+        : null}
+        
+        
+        {!isFetching && posts.length === 0 ? 
+            <div style={{textAlign: 'center', color: 'white' }}>
+                <h2>There are no posts yet.</h2>
+                <p>Start adding some!</p>
+            </div>
+        : null}
+
+        {isFetching && posts.length === 0 ? 
+            <div style={{textAlign: 'center', color: 'white' }}>
+                <h2>Loading Posts...</h2>
+                <p>Please wait</p>
+            </div>
+        : null}
         </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import NewPost from './components/NewPost.jsx'
+
+const router = createBrowserRouter([
+  { path : '/', element : <App/>},
+  { path : '/home', element : <App />},
+  { path : '/create-post', element : <NewPost/>},
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
